9ea1e1ffc4f1a65cb84f73bd7841b17ccd18454c
nt1dc <nt1dcare@gmail.com>
2023-08-07 00:54:32 +0300

    REFACTOR: delete unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
-val exposed_version: String by project
-val h2_version: String by project
 val koin_version: String by project
 plugins {
     kotlin("jvm") version "1.9.0"
@@ -30,10 +28,6 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-freemarker:$ktor_version")
